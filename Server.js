daa2eaf032d434baa2c5d0c60dcbeb8495dc3b38
Srini <srinivasank.23eie@kongu.edu>
2025-06-26 09:23:49 +0530

    mongo url

--- Server.js
+++ Server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 console.log("🛠️ server.js started");
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://srini:12345@daily-journal.pnx5yun.mongodb.net/KEC', {
 }).then(() => console.log('✅ MongoDB connected'))
   .catch(err => console.error('❌ Mongo error:', err.message));
 
